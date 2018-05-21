386ca05ae4ef9018bf1937d7e005dfc9f3d8cc60
ForiDunk <Szabolcs.forreiter@gmail.com>
2018-05-21 06:38:53 +0300

    First commit

---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,27 @@
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        }
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide Details' : 'Show Details'}</button>
+                {this.state.visibility && <p>This is the details I want to toggle</p>}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,25 @@
+// function square(x) {
+//     return x * x;
+// };
+
+// // const squareArrow = (x) => {
+// //     return x * x;
+// // };
+
+// const squareArrow = (x) => x * x;
+
+// console.log(square(3));
+// console.log(squareArrow(4));
+
+// challenge - use arrow function
+// getFirstName('Mike Smith') -> 'Mike'
+// create regular arrow function
+// create arrow function using shorthand syntax
+
+// const getFirstName = (fullName) => {
+//     return fullName.split(' ')[0];
+// }
+
+const getFirstName = (fullName) => fullName.split(' ')[0];
+
+console.log(getFirstName('Mike Smith'));
