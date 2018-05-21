7425ff176cd5c5061bb6c0c2500093346f11517c
Szabolcs Forreiter <szabolcs.forreiter@gmail.com>
2018-05-21 12:24:04 +0000

    set up button styles

--- src/components/Option.js
+++ src/components/Option.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Option = (props) => (
     <div>
-       {props.optionText}
-       <button 
+        {props.optionText}
+        <button 
+            className="button button--link"
             onClick={(event) => {
                 props.handleDeleteOption(props.optionText);
             }}
